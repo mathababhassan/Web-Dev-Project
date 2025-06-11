3e86ebacb1b23bf3009bbf0affad346aff85758f
Leena Ataelmanan <164895009+Leena17111@users.noreply.github.com>
2025-06-11 22:51:41 +0800

    Create forgot_password.php

---
+++ forgot_password.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Forgot Password</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+
+    <h1>Forgot Password</h1>
+
+    <form method="post" action="send_password_reset.php">
+
+        <label for="email">email</label>
+        <input type="email" name="email" id="email" required>
+
+        <button>Send</button>
+
+    </form>
+
+</body>
+</html>
