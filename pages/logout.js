c708ee6e4b5b511f0af7cc047f38a6c469ef280c
Leena Ataelmanan <164895009+Leena17111@users.noreply.github.com>
2025-06-17 15:56:45 +0800

    Delete pages/logout.js

--- pages/logout.js
+++
@@ -1,9 +0,0 @@
-const logoutLink = document.querySelector('a[href="logout.php"]');
-
-if (logoutLink) {
-  logoutLink.onclick = function (event) {
-    if (!confirm("Are you sure you want to log out?")) {
-      event.preventDefault(); // prevent logout
-    }
-  };
-}
