8fd811f19a24664911429881bf717946726031c7
Leena Ataelmanan <164895009+Leena17111@users.noreply.github.com>
2025-06-17 09:06:06 +0800

    Update send_password_reset.php

--- pages/send_password_reset.php
+++ pages/send_password_reset.php
@@ -1,55 +1,46 @@
 <?php
+session_start();
 include 'connect.php';
-require 'vendor/autoload.php';
-
+require '../vendor/autoload.php';
 use SendGrid\Mail\Mail;
 
 $email = $_POST["email"];
-
 $token = bin2hex(random_bytes(16));
-
 $token_hash = hash("sha256", $token);
+$expiry = date("Y-m-d H:i:s", time() + 60 * 30); // token only valid for 30 minutes
 
-$expiry = date("Y-m-d H:i:s", time() + 60 * 30); // the token will only be valid for 30 minutes
-
-$sql = "UPDATE users
-        SET reset_token_hash = ?,
-            reset_token_expires_at = ?
-        WHERE email = ?";
-
+$sql = "UPDATE users SET reset_token_hash = ?, reset_token_expires_at = ? WHERE email = ?";
 $stmt = $conn->prepare($sql);
-
 $stmt->bind_param("sss", $token_hash, $expiry, $email);
-
 $stmt->execute();
 
-if ($conn->affected_rows) {
-    
-    $resetLink = "http://localhost/WebDevProject/reset_password.php?token=$token";
+if ($stmt->affected_rows > 0) {
+    $resetLink = "http://localhost/WebDevProject/pages/reset_password.php?token=$token";
 
-    $emailToSend = new Mail();
-    $emailToSend->setFrom("mindly.service@gmail.com", "Mindly");
-    $emailToSend->addTo($email);
-    $emailToSend->setSubject("Mindly Password Reset");
-    $emailToSend->addContent(
-        "text/html",
-        "Click the link below to reset your password. It expires in 30 minutes:<br><br>
-        <a href='$resetLink'>$resetLink</a>"
-    );
-
-    $sendgrid = new \SendGrid(SENDGRID_API_KEY);
+    $mail = new Mail();
+    $mail->setFrom("mindly.service@gmail.com", "Mindly");
+    $mail->addTo($email);
+    $mail->setSubject("Mindly Password Reset");
+    $mail->addContent("text/html", "
+        Click the link below to reset your password:<br><br>
+        <a href='$resetLink'>$resetLink</a><br><br>
+        This link will expire in 30 minutes.");
 
     try {
-        $response = $sendgrid->send($emailToSend);
+        $sendgrid = new \SendGrid(SENDGRID_API_KEY);
+        $response = $sendgrid->send($mail);
         if ($response->statusCode() === 202) {
-            echo "Password reset email sent. Please check your inbox.";
+            $_SESSION['reset_success'] = "Password reset email sent. Please check your inbox.";
         } else {
-            echo "Failed to send email. Code: " . $response->statusCode();
+            $_SESSION['reset_error'] = "Failed to send email. Please try again later.";
         }
     } catch (Exception $e) {
-        echo "SendGrid error: " . $e->getMessage();
+        $_SESSION['reset_error'] = "SendGrid error: " . $e->getMessage();
     }
-    
 } else {
-    echo "Email not found or update failed.";
+    $_SESSION['reset_error'] = "Email not found. Please try again.";
 }
+
+// Always redirect back to the form page
+header("Location: forgot_password.php");
+exit();
